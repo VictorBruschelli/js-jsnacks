935be5943a4aaa4f30e1cec5955788b226e7334c
bruschelli <infovictorbruschelli@gmail.com>
2024-05-17 23:14:08 +0200

    snack1

---
+++ snack1/main.js
@@ -0,0 +1,12 @@
+const numero1 = prompt('Inserisci il tuo numero1');
+const numero2 = prompt('Inserisci il tuo numero2');
+
+
+if (numero1 < numero2){
+console.log ('il numero maggiore è '+ numero2);
+}
+else if (numero1 > numero2){
+console.log ('il numero maggiore è ' + numero1);
+}
+else (numero1 == numero2) 
+    console.log('i due numeri sono uguali');
