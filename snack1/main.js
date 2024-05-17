3bdbdd3fb995dd23dba0b6b55a305bdca7f65694
bruschelli <infovictorbruschelli@gmail.com>
2024-05-17 23:23:13 +0200

    snack1Correzione

--- snack1/main.js
+++ snack1/main.js
@@ -1,7 +1,15 @@
+
+//chiede all'utente di inserire il primo numero
 const numero1 = prompt('Inserisci il tuo numero1');
+
+//chiede all'utente di inserire il secondo numero
 const numero2 = prompt('Inserisci il tuo numero2');
 
+//converte i valori inseriti in numeri e li transforma in interi (senza virgola)
+numero1 = parseFloat(numero1);
+numero2 = parsefloat(numero2);
 
+//confonta i numeri e stampa il maggiore
 if (numero1 < numero2){
 console.log ('il numero maggiore è '+ numero2);
 }
